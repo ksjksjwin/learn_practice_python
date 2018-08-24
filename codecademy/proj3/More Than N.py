545d32d8447d9e1b9cfd55ee7216b99110928c90
SUNGJIN KIM <ksjksjwin@berkeley.edu>
2018-08-23 21:48:30 -0700

    Create More Than N.py

---
+++ codecademy/proj3/More Than N.py
@@ -0,0 +1,16 @@
+"""
+CODE CHALLENGE: LISTS
+More Than N
+more_than_n(lst, item, n)
+"""
+
+#Write your function here
+
+def more_than_n(lst,item,n):
+  if lst.count(item) > n:
+    return True
+  else:
+    return False
+
+#Uncomment the line below when your function is done
+#print(more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 2, 3))
