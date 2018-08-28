ab309d8f3851c9900a3afbc205200a903dcc2866
SUNGJIN KIM <ksjksjwin@berkeley.edu>
2018-08-28 08:07:19 -0700

    Create Exponents.py

---
+++ codecademy/proj3/Exponents.py
@@ -0,0 +1,18 @@
+"""
+CODE CHALLENGE: LOOPS
+Exponents
+exponents(bases, powers)
+"""
+
+#Write your function here
+def exponents(bases,powers):
+  new_lst = []
+  
+  for base in bases:
+    for power in powers:
+      new_lst.append(base**power)
+  return new_lst
+  
+
+#Uncomment the line below when your function is done
+print(exponents([2, 3, 4], [1, 2, 3]))
