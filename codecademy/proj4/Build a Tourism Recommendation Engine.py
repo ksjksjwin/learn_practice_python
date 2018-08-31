f4ae771820597e2e0793c913095867870413c41a
SUNGJIN KIM <ksjksjwin@berkeley.edu>
2018-08-31 12:49:07 -0700

    Update Build a Tourism Recommendation Engine.py

--- codecademy/proj4/Build a Tourism Recommendation Engine.py
+++ codecademy/proj4/Build a Tourism Recommendation Engine.py
@@ -62,5 +62,22 @@ def find_attractions(destination, interests):
         attractions_with_interest.append(possible_attraction[0])
   return attractions_with_interest
   
-la_arts = find_attractions("Los Angeles, USA",['art'])
-print(la_arts)  
+#la_arts = find_attractions("Los Angeles, USA",['art'])
+#print(la_arts)  
+
+def get_attractions_for_traveler(traveler):
+  traveler_destination = traveler[1]
+  traveler_interests = traveler[2]
+  traveler_attractions = find_attractions(traveler_destination, traveler_interests)
+  interests_string = "Hi " + traveler[0] + ", we think you'll like these places around " + traveler_destination + ": "
+  
+  for attraction in traveler_attractions:
+    if traveler_attractions[-1] == attraction:
+    	interests_string = interests_string + "The " + attraction + "."
+    else:
+      interests_string = interests_string + "The "+ attraction + ","
+  return interests_string
+
+smills_france = get_attractions_for_traveler(['Dereck Smill', 'Paris, France', ['monument']])
+
+#print(smills_france)
