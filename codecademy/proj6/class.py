4b36fa806bda337ed4036839ab854de132406e9f
SUNGJIN KIM <ksjksjwin@berkeley.edu>
2018-09-09 20:49:27 -0700

    Create class.py

---
+++ codecademy/proj6/class.py
@@ -0,0 +1,12 @@
+"""
+- A class is a template for a data type. It describes the kinds of information that class will hold and how a programmer will interact with that data
+- A class instance is also called an object. The pattern of defining classes and creating objects to represent the responsibilities of a program is known as Object Oriented Programming or OOP.
+
+Instantiation takes a class and turns it into an object, the type() function does the opposite of that. When called with an object, it returns the class that the object is an instance of.
+
+print(type(cool_instance))
+# prints "<class '__main__.CoolClass'>"
+We then print out the type() of cool_instance and it shows us that this object is of type __main__.CoolClass.
+
+In Python __main__ means "this current file that we're running" and so one could read the output from type() to mean "the class CoolClass that was defined here, in the script you're currently running."
+"""
