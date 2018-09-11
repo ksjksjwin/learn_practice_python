7867c6bee8c1857c909e0a28ce563afe0c8351fb
SUNGJIN KIM <ksjksjwin@berkeley.edu>
2018-09-10 20:56:15 -0700

    Update class.py

--- codecademy/proj6/class.py
+++ codecademy/proj6/class.py
@@ -15,4 +15,9 @@ In Python __main__ means "this current file that we're running" and so one could
 5. Methods that are used to prepare an object being instantiated are called constructors. The word "constructor" is used to describe similar features in other object-oriented programming languages but programmers who refer to a constructor in Python are usually talking about the __init__ method.
 6. class is a schematic for a data type and an object is an instance of a class
 7. The data held by an object is referred to as an instance variable. Instance variables aren't shared by all instances of a class — they are variables that are specific to the object they are attached to
+8. hasattr(attributeless, "fake_attribute")
+# returns False
+
+getattr(attributeless, "other_fake_attribute", 800)
+# returns 800, the default value
 """
