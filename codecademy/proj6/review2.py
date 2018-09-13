3315226fba709fc86f26fbc55cfb805cfa334333
SUNGJIN KIM <ksjksjwin@berkeley.edu>
2018-09-12 22:08:44 -0700

    Create review2.py

---
+++ codecademy/proj6/review2.py
@@ -0,0 +1,6 @@
+How to create a subclass of an existing class.
+How to redefine existing methods of a parent class in a subclass by overriding them.
+How to leverage a parent class's methods in the body of a subclass method using the super() function.
+How to define a Python exception that inherits from Exception.
+How to write programs that are flexible using interfaces and polymorphism.
+How to write data types that look and feel like native data types with dunder methods.
