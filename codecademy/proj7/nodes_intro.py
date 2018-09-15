878a9c35317501ffed35e8a23cdef163ca85f134
SUNGJIN KIM <ksjksjwin@berkeley.edu>
2018-09-15 12:01:23 -0700

    Update nodes_intro.py

--- codecademy/proj7/nodes_intro.py
+++ codecademy/proj7/nodes_intro.py
@@ -1,1 +1,5 @@
+"""
+Nodes are the fundamental building block of many computer science data structures. They form the basis for linked lists, stacks, queues, trees, and more.
 
+An individual node contains data and links to other nodes. Each data structure adds additional constraints or behavior to these features to create the desired structure.
+"""
