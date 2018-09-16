693b1b3969358d940096cf1965952a6efaea6626
SUNGJIN KIM <ksjksjwin@berkeley.edu>
2018-09-16 10:34:48 -0700

    Update nodes_intro.py

--- codecademy/proj7/nodes_intro.py
+++ codecademy/proj7/nodes_intro.py
@@ -18,12 +18,15 @@ Summary:
 """
 
 class Node:
-  def __init__(self,value,link_node = None):
+  def __init__(self, value, link_node=None):
     self.value = value
     self.link_node = link_node
     
-  def get_value(self):
-    return self.value
-  
+  def set_link_node(self, link_node):
+    self.link_node = link_node
+    
   def get_link_node(self):
     return self.link_node
+  
+  def get_value(self):
+    return self.value
