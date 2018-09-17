41102e0eb45921676588476199f7584f6fa424f0
SUNGJIN KIM <ksjksjwin@berkeley.edu>
2018-09-16 18:26:23 -0700

    Create stack.py

---
+++ codecademy/proj7/stack.py
@@ -0,0 +1,12 @@
+"""
+Stacks:
+1.Contain data nodes
+2.Support three main operations
+  -Push adds data to the top of the stack
+  -Pop removes and provides data from the top of the stack
+  -Peek reveals data on the top of the stack
+3.Implementations include a linked list or array
+4.Can have a limited size
+  -Pushing data onto a full stack results in a stack overflow
+5.Stacks process data Last In, First Out (LIFO)
+"""
