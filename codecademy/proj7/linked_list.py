8f3c773947a30db2a31e1b498242b33eadc79f34
SUNGJIN KIM <ksjksjwin@berkeley.edu>
2018-09-16 17:01:18 -0700

    Create linked_list.py

---
+++ codecademy/proj7/linked_list.py
@@ -0,0 +1,9 @@
+Linked Lists:
+
+1.Are comprised of nodes
+2.The nodes contain a link to the next node (and also the previous node for bidirectional linked lists)
+3.Can be unidirectional or bidirectional
+4.Are a basic data structure, and form the basis for many other data structures
+5.Have a single head node, which serves as the first node in the list
+6.Require some maintenance in order to add or remove nodes
+7.The methods we used are an example and depend on the exact use case and/or programming language being used
