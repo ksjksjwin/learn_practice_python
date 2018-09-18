e08b422f17fd3eca651fd0b3c317654d6af586a6
SUNGJIN KIM <ksjksjwin@berkeley.edu>
2018-09-17 21:12:36 -0700

    Create queue.py

---
+++ codecademy/proj7/queue.py
@@ -0,0 +1,13 @@
+"""
+Queues:
+
+1. Contain data nodes
+2. Support three main operations:
+   - Enqueue adds data to the back of the queue
+   - Dequeue removes and provides data from the front of the queue
+   - Peek provides data on the front of the queue
+4. Can be implemented using a linked list or array
+5. Bounded queues have a limited size.
+6. Enqueueing onto a full queue causes a queue overflow
+7. Queues process data First In, First Out (FIFO)
+"""
