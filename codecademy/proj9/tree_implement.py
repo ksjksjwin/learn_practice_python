34e6ac5961b322dae244501d84a998b5e419e8c1
SUNGJIN KIM <ksjksjwin@berkeley.edu>
2018-09-20 13:03:32 -0700

    Create tree_implement.py

---
+++ codecademy/proj9/tree_implement.py
@@ -0,0 +1,32 @@
+"""
+- Trees are a Python class called TreeNode.
+- A TreeNode has two properties, value and children.
+- Nodes hold any type of data inside value.
+- children is a list, which can be empty or hold other instances of TreeNode.
+- We add to children by using the list method .append.
+- We remove from children by filtering the list.
+"""
+
+class TreeNode:
+  def __init__(self, value):
+    self.value = value # data
+    self.children = [] # references to other nodes
+
+  def add_child(self, child_node):
+    # creates parent-child relationship
+    print("Adding " + child_node.value)
+    self.children.append(child_node) 
+    
+  def remove_child(self, child_node):
+    # removes parent-child relationship
+    print("Removing " + child_node.value + " from " + self.value)
+    self.children = [child for child in self.children 
+                     if child is not child_node]
+
+  def traverse(self):
+    # moves through each node referenced from self downwards
+    nodes_to_visit = [self]
+    while len(nodes_to_visit) > 0:
+      current_node = nodes_to_visit.pop()
+      print(current_node.value)
+      nodes_to_visit += current_node.children
